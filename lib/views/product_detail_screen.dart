8bfd8d499c2303ed3cc7504773b50cc2c76bb923
Lincoln Arrais <lincoln_arrais@hotmail.com>
2020-04-25 22:17:28 -0300

    Inherited Widget na Prática

--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import '../models/product.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-  final Product product = ModalRoute.of(context).settings.arguments as Product;
+    final Product product = ModalRoute.of(context).settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
