5899078c9b8efca0ceb8bbf3cc1f94c12dd9906f
Lincoln Arrais <lincoln_arrais@hotmail.com>
2020-04-25 21:45:17 -0300

    Navegando para Detalhe de Produto

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'utils/app_routes.dart';
+import 'views/product_detail_screen.dart';
 import 'views/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
@@ -14,6 +16,9 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Lato',
       ),
       home: ProductOverviewScreen(),
+      routes: {
+        AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
+      },
     );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 import '../models/product.dart';
+import '../utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
-  final Product product;
+    final Product product;
 
-  ProductItem(this.product);
+    ProductItem(this.product);
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-        child: Image.network(
-          product.imageUrl,
-          fit: BoxFit.cover,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              AppRoutes.PRODUCT_DETAIL,
+              arguments: product,
+            );
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
